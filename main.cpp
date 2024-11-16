b17f485cf472eda264e78f275d62b918b24932bc
Extremis <leonard.goetz@utbm.fr>
2024-11-16 21:16:44 +0100

    Base

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Déclaration de la structure
+stypedef struct {
+    int *poids;
+    int biais;
+}Neurone;
+
+Neurone InitNeurone (int Poids) {
+    Neurone neurone;
+};
+
+Neurone OutNeurone (int Poids) {
+    Neurone neurone;
+
+};
+
+int main() {
+
+}
