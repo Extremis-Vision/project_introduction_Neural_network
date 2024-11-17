a3cbee02d8bf6d1d973386f2d7f522d895017621
Dridox <bastien.heitzmann@gmail.com>
2024-11-17 16:45:36 +0100

    ajout fonction init

--- main.c
+++ main.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Déclaration de la structure
-stypedef struct {
+
+typedef struct p
+{
+    int valeur;
+    struct suivant* p;
+}poids;
+
+typedef struct {
     int *poids;
     int biais;
 }Neurone;
 
+// fonction qui initilise le neuronne
 Neurone InitNeurone (int Poids) {
     Neurone neurone;
+    
 };
 
 Neurone OutNeurone (int Poids) {
