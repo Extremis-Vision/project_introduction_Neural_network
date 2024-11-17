79768d60a8f85ec6d1f49e488fb4ae23dbbc6ef2
Dridox <bastien.heitzmann@gmail.com>
2024-11-17 17:00:37 +0100

    ajout fonction out

--- main.c
+++ main.c
@@ -4,11 +4,6 @@
 
 // Déclaration de la structure
 
-typedef struct p
-{
-    int valeur;
-    struct suivant* p;
-}poids;
 
 typedef struct {
     int *poids;
@@ -16,14 +11,40 @@ typedef struct {
 }Neurone;
 
 // fonction qui initilise le neuronne
-Neurone InitNeurone (int Poids) {
+Neurone InitNeurone (int nbPoids) {
     Neurone neurone;
+
+    // allocation mémoire du neuronne
+    neurone.poids = (int*)malloc(nbPoids * sizeof(int));
+    if (neurone.poids == NULL) {
+        perror("Erreur d'allocation mémoire");
+        exit(EXIT_FAILURE);
+    }
+
+    // début de l'allocation aléatoire des poids et biais
+    srand(time(NULL));
+    for (int i = 0; i < nbPoids; i++) {
+        neurone.poids[i] = rand() % 10; 
+    }
+    neurone.biais = rand() % 10;
+
+
+    return neurone;
+
     
 };
 
-Neurone OutNeurone (int Poids) {
-    Neurone neurone;
+int  OutNeurone (Neurone neurone, int *ei, int nbPoids) {
+    int somme = 0;
 
+for (int i = 0; i < nbPoids; i++) {
+    if (ei[i] == 1){
+          return 1;
+        }else {
+             somme += ei[i] * neurone.poids[i]; // Calcul de la somme pondérée
+        }   
+    }
+    return somme;
 };
 
 int main() {
